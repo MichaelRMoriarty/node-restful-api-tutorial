645bb9dafea8f70ec5a1d3e478ce895d981621ad
Michael Moriarty <mmoriarty316@gmail.com>
2021-08-04 14:50:09 -0700

    added CORS

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((res, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+})
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
